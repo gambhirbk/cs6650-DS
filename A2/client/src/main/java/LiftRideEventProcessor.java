7dcaa829ef3cafc330cb7a21b17bf1ebd3d8591b
Gambhir Kunwar <gambhirkunwar@Gambhirs-MacBook-Pro.local>
2022-10-26 13:40:40 -0700

    worked on finishing up client 1

---
+++ A2/client/src/main/java/LiftRideEventProcessor.java
@@ -0,0 +1,84 @@
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiResponse;
+import io.swagger.client.api.SkiersApi;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+
+public class LiftRideEventProcessor extends Thread {
+
+    private static final Integer MAX_RETRIES = 5;
+    private final BlockingQueue<LiftRideEvent> sharedQueue;
+    private CountDownLatch latch;
+    private CountDownLatch overallLatch;
+
+    private SharedResults sharedResults;
+
+    private static final Integer NUMBER_OF_REQUESTS = 1000;
+
+    public LiftRideEventProcessor(BlockingQueue sharedQueue, CountDownLatch latch, CountDownLatch overallLatch, SharedResults sharedResults) {
+        this.sharedQueue = sharedQueue;
+        this.latch = latch;
+        this.overallLatch = overallLatch;
+        this.sharedResults = sharedResults;
+    }
+
+    public void run() {
+        int successfulPosts = 0;
+        int failedPosts = 0;
+        String basePath = "http://35.92.155.138:8080/server_war_exploded";
+        SkiersApi apiInstance = new SkiersApi();
+        ApiClient client = apiInstance.getApiClient();
+        client.setBasePath(basePath);
+        for (int i = 0; i < NUMBER_OF_REQUESTS; i++) {
+
+            // maximum of 5 tries
+            boolean success = false;
+            int numTries = 0;
+
+            while (!success && numTries < MAX_RETRIES) {
+                try {
+                    if (Main.q.peek() == null) {
+                        break;
+                    }
+                    LiftRideEvent liftRideEvent = Main.q.take();
+                    ApiResponse<Void> response = apiInstance.writeNewLiftRideWithHttpInfo(liftRideEvent.getLiftRide(), liftRideEvent.getResortID(), liftRideEvent.getSeasonID(),
+                            liftRideEvent.getDayID(), liftRideEvent.getSkierID());
+                    System.out.println(response.getStatusCode());
+                    if (response.getStatusCode() >= 400) {
+                        sleepThread(numTries++);
+                        continue;
+                    }
+                    successfulPosts++;
+                } catch (ApiException e) {
+                    System.out.println(e.getCode());
+                    System.err.println("Exception thrown while calling SkierApi for writeNewLiftRide");
+                    System.err.println(e.getMessage());
+                    failedPosts++;
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+            this.sharedResults.incrementSuccessfulPost(successfulPosts);
+            this.sharedResults.incrementFailedPost(failedPosts);
+
+            try {
+                if (this.latch != null) {
+                    this.latch.countDown();
+                }
+                this.overallLatch.countDown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    private void sleepThread(Integer numTries) {
+        try {
+            Thread.sleep(2^numTries);
+        } catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ A2/server/src/main/java/servlet/SkierServlet.java
@@ -0,0 +1,117 @@
+package servlet;
+
+import com.google.gson.Gson;
+import model.Message;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet(name = "SkierServlet", urlPatterns = "/skiers/*")
+public class SkierServlet extends HttpServlet {
+
+    private Gson gson = new Gson();
+    private String msg;
+
+    private final String SEASONS_PARAMETER = "seasons";
+    private final String DAYS_PARAMETER = "days";
+    private final String SKIERS_PARAMETER = "skiers";
+    private final int DAY_ID_MIN = 1;
+    private final int DAY_ID_MAX = 365;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/plain");
+        String urlPath = req.getPathInfo();
+        System.out.println(urlPath);
+
+        // check we have a URL!
+        if (urlPath == null || urlPath.isEmpty()) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.getWriter().write("404: Missing url");
+            return;
+        }
+
+        String[] urlParts = urlPath.split("/");
+        // and now validate url path and return the response status code
+        // (and maybe also some value if input is valid)
+        if (!isUrlValid(urlParts, req)) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.getWriter().write(msg);
+        } else {
+            res.setStatus(HttpServletResponse.SC_OK);
+            // do any sophisticated processing with urlParts which contains all the url params
+            // TODO: process url params in `urlParts`
+            res.getWriter().write("200: It works!!!!");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        String urlPath = request.getPathInfo();
+
+        if (urlPath == null || urlPath.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("missing parameter");
+        }
+
+        String[] urlParts = urlPath.split("/");
+
+        // and now validate url path and return the response status code
+        // (and maybe also some value if input is valid)
+        BufferedReader buffIn = null;
+
+        try {
+            buffIn = request.getReader();
+            StringBuilder reqBody = new StringBuilder();
+            String line;
+            while ((line = buffIn.readLine()) != null) {
+                reqBody.append(line);
+            }
+            response.getWriter().write(reqBody.toString());
+        } catch (Exception e) {
+            Message message = new Message("string");
+            response.getWriter().write(gson.toJson(message));
+            return;
+        } finally {
+            if (buffIn != null){
+                try {
+                    buffIn.close();
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        if (!isUrlValid(urlParts, request)) {
+            Message message = new Message("string");
+            response.getWriter().write(gson.toJson(message));
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    private boolean isUrlValid(String[] urlPath, HttpServletRequest req) {
+        if (urlPath.length == 8) {
+            try {
+                for (int i = 1; i < urlPath.length; i += 2) {
+                    Integer.parseInt(urlPath[i]);
+                }
+                return (urlPath[3].length() == 4
+                        && Integer.valueOf(urlPath[5]) >= DAY_ID_MIN
+                        && Integer.valueOf(urlPath[5]) < DAY_ID_MAX
+                        && urlPath[2].equals(SEASONS_PARAMETER)
+                        && urlPath[4].equals(DAYS_PARAMETER)
+                        && urlPath[6].equals(SKIERS_PARAMETER));
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
